70bf683df4c312306e10b1c16a3b5261c2c552af
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-08-16 08:17:53 +0100

    finished info1

--- src/components/Info/style.js
+++ src/components/Info/style.js
@@ -1,24 +1,28 @@
 import styled from "styled-components";
 export const InfoWrapper = styled.div`
   /* both */
+  position: relative;
   background-color: white;
   font-family: Overpass;
   font-style: normal;
-  font-weight: bold;
+  display: flex;
   color: #1f3c58;
   /* desktop */
   @media screen and (min-width: 376px) {
-    position: relative;
     width: 1440px;
     height: 1010px;
-    display: flex;
   }
-`;
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    flex-direction: column-reverse;
+  }
+  `;
 export const InfoTitle = styled.h2`
   /* both */
+  position: absolute;
+  font-weight: 900;
   /* desktop */
   @media screen and (min-width: 376px) {
-    position: absolute;
     width: 406px;
     height: 46px;
     left: 527px;
@@ -27,22 +31,39 @@ export const InfoTitle = styled.h2`
     font-size: 37px;
     line-height: 57px;
   }
-`;
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    width: 375px;
+    height: 46px;
+    top: 83px;
+    font-size: 24px;
+    line-height: 37px;
+    text-align: center;
+  }
+  `;
 export const InfoDivider = styled.div`
   /* desktop */
   @media screen and (min-width: 376px) {
     width: 720px;
     height: 1010px;
   }
-`;
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    /* width: 375px; */
+  }
+  `;
 
 export const InfoContent = styled.div`
   /* desktop */
   @media screen and (min-width: 376px) {
-    width: 563px;
-    height: 481px;
-    margin-left: 157px;
-    margin-top: 308px;
+    margin-top: 300px;
+    margin-left: 160px;
+  }
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    width: 375px;
+    height: 828px;
+    margin-top: 30px;
   }
 `;
 
@@ -52,17 +73,53 @@ export const InfoText = styled.div`
     width: 563px;
     height: 255px;
   }
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    text-align: center;
+  }
 `;
 export const InfoSubTitle = styled.h3`
+  /* both */
+  font-weight: 700;
   font-size: 27.5px;
-  line-height: 42px;
-  margin-bottom: 20px;
+  /* desktop */
+  @media screen and (min-width: 376px) {
+    line-height: 42px;
+    margin-bottom: 20px;
+  }
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    position: relative;
+    width: 262px;
+    height: 60px;
+    left: 57px;
+    line-height: 103%;
+    text-align: center;
+  }
 `;
 export const InfoPara = styled.p`
-  font-weight: normal;
-  font-size: 17px;
-  line-height: 26px;
+  /* both */
 
   color: #4b5862;
+  font-weight: normal;
+  /* desktop */
+  @media screen and (min-width: 376px) {
+    font-size: 17px;
+    line-height: 26px;
+  }
+
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    width: 375px;
+    height: 247px;
+    font-size: 15.4px;
+    line-height: 24px;
+    text-align: center;
+  }
 `;
-export const InfoImg = styled.img``;
+export const InfoImg = styled.img`
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    margin-top: 158px;
+  }
+`;
