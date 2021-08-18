73e6766745f41e5ed78d2eaa0566a15afc6d2d57
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-08-18 01:07:17 +0100

    finished footer

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -1,33 +1,60 @@
 import styled from "styled-components";
 export const FooterWrapper = styled.div`
+  /* both */
   position: relative;
-  width: 1440px;
-  height: 360px;
-  background: #24242d;
   border-radius: 0px 100px 0px 0px;
-  padding-top: 70px;
-  padding-left: 160px;
+  background: #24242d;
   display: flex;
-  align-items: flex-start;
-  `;
-  export const FooterLogo = styled.img`
-    width: 101px;
-    height: 40px;
+  /* desktop */
+  @media screen and (min-width: 376px) {
+    width: 1440px;
+    height: 360px;
+    padding-top: 70px;
+    padding-left: 160px;
+    align-items: flex-start;
+  }
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    flex-direction: column;
+    align-items: center;
+    /* text-align: center; */
+    width: 375px;
+    height: 787px;
+  }
+`;
+export const FooterLogo = styled.img`
+  /* both */
+  width: 101px;
+  height: 40px;
+  /* desktop */
+  @media screen and (min-width: 376px) {
     margin-right: 170px;
-    `;
-  export const FooterLinks = styled.div`
+  }
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    margin-top: 65px;
+    margin-bottom: 85px;
+  }
+`;
+export const FooterLinks = styled.div`
+  /* both */
   width: 120px;
-  height: 240px;
-  margin-right: 170px;
-  `
-  export const FooterLink = styled.p`
-    &:first-of-type {
-        margin-bottom: 30px;
-        font-weight: 500;
-    }
-    width: 69px;
-    height: 18px;
-    margin-bottom: 15px;
-    margin-left: 5px;
-    font-weight: 300;
-  `;
+  /* desktop */
+  @media screen and (min-width: 376px) {
+    margin-right: 170px;
+  }
+  /* mobile */
+  @media screen and (max-width: 375px) {
+    margin-bottom: 45px;
+  }
+`;
+export const FooterLink = styled.p`
+  &:first-of-type {
+    margin-bottom: 30px;
+    font-weight: 500;
+  }
+  width: 100%;
+  text-align: center;
+  margin-bottom: 15px;
+  font-weight: 300;
+`;
