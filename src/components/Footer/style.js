1d7bab5b81105abd720ed9090e93a4a064dbbbbc
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-08-18 02:07:57 +0100

    minor changes

--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -52,7 +52,12 @@ export const FooterLink = styled.p`
   &:first-of-type {
     margin-bottom: 30px;
     font-weight: 500;
+    cursor: none; 
   }
+  &:not(:first-of-type):active {
+    text-decoration: underline;
+  }
+  cursor: pointer; 
   width: 100%;
   text-align: center;
   margin-bottom: 15px;
