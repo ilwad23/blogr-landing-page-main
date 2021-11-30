b21ffd1cd06e968eb9fd55f261d83097ad3f63f5
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-11-30 15:04:44 +0000

    better layout

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -1,50 +1,38 @@
 import React from "react";
-import {
-  InfoWrapper,
-  InfoTitle,
-  InfoDivider,
-  InfoContent,
-  InfoText,
-  InfoSubTitle,
-  InfoPara,
-  InfoImg,
-} from "./style.js";
 
 function Info({ desktopOrMobile }) {
   return (
-      <InfoWrapper>
-      <InfoTitle>Designed for the future</InfoTitle>
-        <InfoDivider>
-          <InfoContent>
-            <InfoText>
-              <InfoSubTitle>Introducing an extensible editor</InfoSubTitle>
-              <InfoPara>
-                Blogr features an exceedingly intuitive interface which lets you
-                focus on one thing: creating content. The editor supports
-                management of multiple blogs and allows easy manipulation of
-                embeds such as images, videos, and Markdown. Extensibility with
-                plugins and themes provide easy ways to add functionality or
-                change the looks of a blog.
-              </InfoPara>
-            </InfoText>
-            <InfoText>
-              <InfoSubTitle>Robust content management</InfoSubTitle>
-              <InfoPara>
-                Flexible content management enables users to easily move through
-                posts. Increase the usability of your blog by adding customized
-                categories, sections, format, or flow. With this functionality,
-                you’re in full control.
-              </InfoPara>
-            </InfoText>
-          </InfoContent>
-        </InfoDivider>
-        <InfoDivider>
-          <InfoImg
-            src={`./images/design-illustration-${desktopOrMobile}.svg`}
-            alt="design-illustration"
-          />
-        </InfoDivider>
-      </InfoWrapper>
+    <section className="info l">
+      <h2 className="info__title l">Designed for the future</h2>
+      <img
+        src={`./images/design-illustration-${desktopOrMobile}.svg`}
+        alt="design-illustration"
+      />
+      <div className="info__container l">
+        <div className="info__content">
+          <div className="info__content-1">
+            <h3 className='info__subTitle'>Introducing an extensible editor</h3>
+            <p className="info__para">
+              Blogr features an exceedingly intuitive interface which lets you
+              focus on one thing: creating content. The editor supports
+              management of multiple blogs and allows easy manipulation of
+              embeds such as images, videos, and Markdown. Extensibility with
+              plugins and themes provide easy ways to add functionality or
+              change the looks of a blog.
+            </p>
+          </div>
+          <div className="info__content-2">
+            <h3 className='info__subTitle'>Robust content management</h3>
+            <p className="info__para">
+              Flexible content management enables users to easily move through
+              posts. Increase the usability of your blog by adding customized
+              categories, sections, format, or flow. With this functionality,
+              you’re in full control.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
