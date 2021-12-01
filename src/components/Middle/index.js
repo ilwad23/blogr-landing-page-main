4aee17299ad66b85ecb8c2f19398c8dcd5f8928f
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-12-01 10:55:48 +0000

    changed the layout

--- src/components/Middle/index.js
+++ src/components/Middle/index.js
@@ -1,32 +1,33 @@
-import React from 'react'
-import {
-  MiddleWrapper,
-  MiddleBgCircles,
-  MiddleTitle,
-  MiddlePara,
-  MiddlePhones,
-} from "./style.js";
-import "../../app.css";
+import React from "react";
+
 function Middle({ desktopOrMobile }) {
-    return (
-      <MiddleWrapper>
-        <MiddlePhones
+  return (
+    <section className="middle">
+      <div className="middle__wrapper">
+        <img
+          className="middle__phoneImg"
           src={`./images/phones-${desktopOrMobile}.svg`}
           alt="phones"
         />
-        <MiddleBgCircles
-          src={`./images/bg-circles-${desktopOrMobile}.svg`}
-          alt="bg-circles"
-        />
-        <MiddleTitle>State of the Art Infrastructure</MiddleTitle>
-        <MiddlePara>
-          With reliability and speed in mind, worldwide data centers provide the
-          backbone for ultra-fast connectivity. This ensures your site will load
-          instantly, no matter where your readers are, keeping your site
-          competitive.
-        </MiddlePara>
-      </MiddleWrapper>
-    );
+        <div className="middle__content">
+          <div className="title">
+            State of the Art <span>Infrastructure</span>
+          </div>
+          <div className="para">
+            With reliability and speed in mind, worldwide data centers provide
+            the backbone for ultra-fast connectivity. This ensures your site
+            will load instantly, no matter where your readers are, keeping your
+            site competitive.
+          </div>
+        </div>
+      </div>
+      <img
+        className="middle__bgCirclesImg"
+        src={`./images/bg-circles-${desktopOrMobile}.svg`}
+        alt="bg-circles"
+      />
+    </section>
+  );
 }
 
-export default Middle
+export default Middle;
