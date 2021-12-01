27aac7b0975669a2125483441ab64277a232302b
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-12-01 11:20:57 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Header from "./components/Header";
-import Info from "./components/Info";
+import Info from "./components/Info2";
 import Middle from "./components/Middle";
 import "./app.css";
 
@@ -22,12 +22,6 @@ function App() {
       <Header desktopOrMobile={desktopOrMobile} />
       <Info
         desktopOrMobile={desktopOrMobile}
-        title="Designed for the future"
-        subtitle1="Introducing an extensible editor"
-        para1="Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or change the looks of a blog."
-        subtitle2="Robust content management"
-        para2="Flexible content management enables users to easily move through posts. Increase the usability of your blog by adding customized categories, sections, format, or flow. With this functionality, you’re in full control."
-        img="design-illustration"
       />
       <Middle desktopOrMobile={desktopOrMobile} />
       <Info
