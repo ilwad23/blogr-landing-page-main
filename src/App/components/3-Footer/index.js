d8f3c4cdbee7a9dd39ac92d9cb476ab0812f7f3b
Ilwad Abukor <ilwadabukor@hotmail.com>
2021-12-01 20:51:10 +0000

    finished convert to sass

--- src/App/components/3-Footer/index.js
+++ src/App/components/3-Footer/index.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { FooterWrapper, FooterLogo, FooterLinks, FooterLink } from "./style.js";
+
 function Footer() {
   return (
     <footer className='footer'>
       <img className="footer__logo" src="./images/logo.svg" alt="logo" />
       <ul className="footer__links">
-        <li className="footer__link">Overview</li>
+        <li className="footer__link__title">Overview</li>
         <li className="footer__link">Pricing</li>
         <li className="footer__link">Marketplace</li>
         <li className="footer__link">Features</li>
         <li className="footer__link">Integration</li>
       </ul>
-      <ul className="footer__links">
-        <li className="footer__link">Team</li>
+      <ul className="footer__links ">
+        <li className="footer__link__title">Team</li>
         <li className="footer__link">About</li>
         <li className="footer__link">Blog</li>
         <li className="footer__link">Careers</li>
       </ul>
       <ul className="footer__links">
-        <li className="footer__link">Company</li>
+        <li className="footer__link__title">Company</li>
         <li className="footer__link">Contact</li>
         <li className="footer__link">Newsletter</li>
         <li className="footer__link">Linkedin</li>
